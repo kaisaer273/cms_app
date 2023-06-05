9e5b7e3301e45d6470f64b472c2130d2fb30f11d
kaisaer273 <dvietanh273@gmail.com>
2023-06-05 16:52:54 +0700

    05/6.1

--- lib/views/widgets/medicine_widget.dart
+++ lib/views/widgets/medicine_widget.dart
@@ -4,40 +4,42 @@ import 'package:get/get.dart';
 import 'package:cms_app/controllers/cart_controller.dart';
 
 class MedicineWidget extends StatelessWidget {
-final cartController = Get.put(CartController());
+  final cartController = Get.put(CartController());
 
-   MedicineWidget({super.key});
+  MedicineWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children:[Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  onChanged: (value) {
-                    cartController.filterMedicine(value);
-                  },
-                  style: const TextStyle(color: Colors.black),
-                  decoration: const InputDecoration(
-                    labelText: 'Search',
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Obx(() {
-                  final filteredMedicineList = cartController.filteredMedicineList;
-                  return ListView.builder(
-              itemCount: filteredMedicineList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return MedicineCard(index: index, cartController: cartController, filteredMedicineList: filteredMedicineList,);
-              });
-                }),
-              ),] 
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) {
+              cartController.filterMedicine(value);
+            },
+            style: const TextStyle(color: Colors.black),
+            decoration: const InputDecoration(
+              labelText: 'Search',
+            ),
+          ),
         ),
-      ),
+        Expanded(
+          child: Obx(() {
+            final filteredMedicineList = cartController.filteredMedicineList;
+            return ListView.builder(
+                itemCount: filteredMedicineList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return MedicineCard(
+                    index: index,
+                    cartController: cartController,
+                    filteredMedicineList: filteredMedicineList,
+                  );
+                });
+          }),
+        ),
+      ]),
     );
   }
 }
@@ -46,30 +48,34 @@ class MedicineCard extends StatelessWidget {
   final int index;
   final CartController cartController;
   final filteredMedicineList;
-  const MedicineCard({super.key, required this.index, required this.cartController, required this.filteredMedicineList});
+  const MedicineCard(
+      {super.key,
+      required this.index,
+      required this.cartController,
+      required this.filteredMedicineList});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-          leading: const Icon(Icons.medication),
-          title: Text(filteredMedicineList[index].genericName),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(filteredMedicineList[index].activeIngredientName),
-              Row(
-                children: [
-                  Text("${filteredMedicineList[index].priceOut3} VND"),
-                  const Text("/"),
-                  Text(filteredMedicineList[index].volume3.toString())
-                ],
-              ),
-            ],
-          ),
-          trailing: Obx(()=>GestureDetector(
+        leading: const Icon(Icons.medication),
+        title: Text(filteredMedicineList[index].genericName),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(filteredMedicineList[index].activeIngredientName),
+            Row(
+              children: [
+                Text("${filteredMedicineList[index].priceOut3} VND"),
+                const Text("/"),
+                Text(filteredMedicineList[index].volume3.toString())
+              ],
+            ),
+          ],
+        ),
+        trailing: Obx(() => GestureDetector(
               onTap: () {
-               cartController.addMedicine(filteredMedicineList[index]);
+                cartController.addMedicine(filteredMedicineList[index]);
               },
               child: (cartController.medicinesInCart
                       .containsKey(filteredMedicineList[index]))
@@ -77,8 +83,9 @@ class MedicineCard extends StatelessWidget {
                       Icons.assignment_add,
                       color: Colors.blue,
                     )
-                  : const Icon(Icons.assignment_add),)
-            ),),
+                  : const Icon(Icons.assignment_add),
+            )),
+      ),
     );
   }
 }
--- lib/views/widgets/medicines_in_cart.dart
+++ lib/views/widgets/medicines_in_cart.dart
@@ -11,20 +11,29 @@ class MedicinesInCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() => Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: cartController.medicinesInCart.length>0 ? ListView.builder(
-            itemCount: cartController.medicinesInCart.length,
-            itemBuilder: (BuildContext context, int index) {
-              return MedicineInCartCard(
-                cartController: cartController,
-                medicine: cartController.medicinesInCart.keys.toList()[index],
-                index: index,
-                quantity: cartController.medicinesInCart.values.toList()[index],
-              );
-            }) : const Center(child: Text("Chưa có thuốc",style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 152, 147, 135)),),),
-    )
-    
-    );
+          padding: const EdgeInsets.all(8.0),
+          child: cartController.medicinesInCart.length > 0
+              ? ListView.builder(
+                  itemCount: cartController.medicinesInCart.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return MedicineInCartCard(
+                      cartController: cartController,
+                      medicine:
+                          cartController.medicinesInCart.keys.toList()[index],
+                      index: index,
+                      quantity:
+                          cartController.medicinesInCart.values.toList()[index],
+                    );
+                  })
+              : const Center(
+                  child: Text(
+                    "Chưa có thuốc",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 152, 147, 135)),
+                  ),
+                ),
+        ));
   }
 }
 
@@ -42,23 +51,116 @@ class MedicineInCartCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return Card(
+    //   child: ListTile(
+    //     leading: Text('${index+1}'),
+    //     title: Text(medicine.genericName),
+    //     subtitle: Text(medicine.activeIngredientName),
+    //     trailing: SizedBox(
+    //       width: context.width * 0.3,
+    //       height: double.infinity,
+    //       child: Row(
+    //         mainAxisAlignment: MainAxisAlignment.end,
+    //         children: [
+    //           IconButton(
+    //               onPressed: () {cartController.decreMedicine(medicine);}, icon: const Icon(Icons.remove_circle)),
+    //           Text('$quantity'.padLeft(2,'0')),
+    //           IconButton(onPressed: () {cartController.increMedicine(medicine);}, icon: const Icon(Icons.add_circle))
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
     return Card(
-      child: ListTile(
-        leading: Text('${index+1}'),
-        title: Text(medicine.genericName),
-        subtitle: Text(medicine.activeIngredientName),
-        trailing: SizedBox(
-          width: context.width * 0.3,
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                  onPressed: () {cartController.decreMedicine(medicine);}, icon: const Icon(Icons.remove_circle)),
-              Text('$quantity'.padLeft(2,'0')),
-              IconButton(onPressed: () {cartController.increMedicine(medicine);}, icon: const Icon(Icons.add_circle))
-            ],
-          ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    flex: 15,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          medicine.genericName,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(medicine.activeIngredientName,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 16,
+                            )),
+                        Row(
+                          children: [
+                            const Expanded(flex: 1, child: Text("Số lượng")),
+                            Expanded(
+                              flex: 1,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        cartController.decreMedicine(medicine);
+                                      },
+                                      icon: const Icon(
+                                        Icons.remove_circle,
+                                        color: Colors.black54,
+                                      )),
+                                  Text(
+                                    '$quantity'.padLeft(2, '0'),
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  IconButton(
+                                      onPressed: () {
+                                        cartController.increMedicine(medicine);
+                                      },
+                                      icon: const Icon(
+                                        Icons.add_circle,
+                                        color: Colors.black54,
+                                      ))
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: Container(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(medicine.volume3)))
+                          ],
+                        ),
+                        Container(
+                            alignment: Alignment.centerRight,
+                            child:
+                                Text("Giá: ${medicine.priceOut3 * quantity}"))
+                      ],
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: GestureDetector(
+                        onTap: () {
+                          cartController.removeMedicine(medicine);
+                        },
+                        child: const Icon(Icons.close)))
+              ],
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "Ghi chú",
+                isDense: true,
+              ),
+            )
+          ],
         ),
       ),
     );
